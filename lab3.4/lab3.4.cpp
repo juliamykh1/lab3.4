4c546367ec6019d6090a2e21f0c3b69b79644c0c
Julia <mihajloviculia65@gmail.com>
2022-10-27 13:49:42 +0300

    first master commit

---
+++ lab3.4/lab3.4.cpp
@@ -0,0 +1,24 @@
+﻿// Lab_03_4.cpp
+// < прізвище, ім’я автора >
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 0.1
+#include <iostream>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double y;
+	double R; // вхідний параметр
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "R = "; cin >> R;
+
+	// розгалуження в повній формі
+	if (y=abs( R-x) || y >= -R && y >= abs(R - x))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+}
